680a556a5f895650418a68c4d9b39da1abc8dbef
Nick Kelley <nickdk.1995@gmail.com>
2017-01-12 11:53:57 -0500

    wrote method to convert date to english month year

--- web/views/pdf_view.ex
+++ web/views/pdf_view.ex
@@ -1,11 +1,26 @@
 defmodule Resume.PdfView do
   use Resume.Web, :view
 
+  @months ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
   def skill_columns(skills) do
     forth = (Enum.count(skills) / 4) |> Float.ceil |> trunc
     {c1,c234} = Enum.split(skills, forth)
     {c2,c34} = Enum.split(c234, forth)
     {c3,c4} = Enum.split(c34, forth)
     {c1,c2,c3,c4}
+  end
+
+  def experience_rows(experiences) do
+    work = Enum.filter(experiences, &(&1.category == "Work"))
+    volunteer = Enum.filter(experiences, &(&1.category == "Volunteer"))
+    project = Enum.filter(experiences, &(&1.category == "Project"))
+    {work, volunteer, project}
+  end
+
+  def english_date(date) do
+    [_, year, month] = Regex.run(~r/^(\d{4})-(\d{1,2})/, to_string(date))
+    index = String.to_integer(month) - 1
+    "#{Enum.at(@months, index)} #{year}"
   end
 end
