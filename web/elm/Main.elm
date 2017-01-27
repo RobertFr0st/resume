7f3fa247e56cc3062e720917234f978874284bf6
Nick Kelley <nickdk.1995@gmail.com>
2017-01-27 11:16:36 -0500

    finished elm, now only preloads on pdf new page

--- web/elm/Main.elm
+++ web/elm/Main.elm
@@ -1,11 +1,9 @@
 module Main exposing (..)
 
 import Html exposing (Html, button, div, text)
-import Html.App as Html
 import Html.Events exposing (onClick)
 
-
-main : Program Never
+main : Program Never Model Msg
 main =
     Html.beginnerProgram { model = model, view = view, update = update }
 
