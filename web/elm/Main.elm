514b15c69f136b4e6ab115dcec7785c2ee295699
Nick Kelley <nickdk.1995@gmail.com>
2017-02-09 10:34:43 -0500

    styled elm page

--- web/elm/Main.elm
+++ web/elm/Main.elm
@@ -1,29 +1,36 @@
 module Main exposing (..)
 
 import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (property)
 import Html.Events exposing (onClick)
+import Json.Encode as Json exposing (string)
 
 main : Program Never Model Msg
 main =
     Html.beginnerProgram { model = model, view = view, update = update }
 
-
-
 -- MODEL
 
+type alias Skill = 
+  { id : Int
+  , name : String
+  , user_id : Int
+  --, inserted_at : 
+  --, updated_at : 
+  }
 
 type alias Model =
-    Int
+   List Skill
+ 
 
 
-model : Model
-model =
-    0
+model : Model -> Model
+model skills =
+    skills
 
 
 
 -- UPDATE
-
 
 type Msg
     = Increment
@@ -32,22 +39,24 @@ type Msg
 
 update : Msg -> Model -> Model
 update msg model =
-    case msg of
-        Increment ->
-            model + 1
+  case msg of
+    Increment ->
+      model
 
-        Decrement ->
-            model - 1
+    Decrement ->
+      model
 
 
 
 -- VIEW
 
-
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (toString model) ]
-        , button [ onClick Increment ] [ text "+" ]
-        ]
+  div [ property "className" (Json.string "row justify-content-center") ]
+    [
+      div [ property "className" (Json.string "col-6") ]
+      [ text "test" ],
+
+      div [ property "className" (Json.string "col-6") ]
+      [ text "test" ]
+    ]
