e379f67d913b7abe78c8d4b326d9369966e33684
Nicholas Kelley <nickdk.1995@gmail.com>
2018-07-21 17:33:35 -0400

    typ

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,7 @@ config :resume, Resume.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "fierce-brook-69606.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :resume, Resume.Repo,
